f34dc61c33f327bb4c8816cf1b82e5424d5ef9dc
isshosng <purehug@hanmail.net>
2022-08-06 15:41:25 +0900

    수정 구현

--- src/main/java/com/posting2/board/BoardApplication.java
+++ src/main/java/com/posting2/board/BoardApplication.java
@@ -2,9 +2,11 @@ package com.posting2.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
-@EnableJpaAuditing // JPA Auditing 기능을 사용하기 위해 main 클래스에 해당 어노테이션을 붙여줌
+@EnableJpaAuditing // JPA Auditing 기능을 사용하기 위해 main 클래스에 해당 어노테이션을 붙여
 @SpringBootApplication
 public class BoardApplication {
 
@@ -13,4 +15,8 @@ public class BoardApplication {
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
 }
--- src/main/java/com/posting2/board/Controller/BoardController.java
+++ src/main/java/com/posting2/board/Controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 
@@ -21,7 +22,6 @@ public class BoardController {
     public BoardController(BoardService boardService){
         this.boardService = boardService;
     }
-
 
     /*
     게시물의 목록을 가져오는 getBoardList()를 만들었으므로, 가져온 데이터를 통해 View에 전달해야 함
@@ -56,4 +56,23 @@ public class BoardController {
     BoardController에 detail()을 아래와 같이 구현하며, 요청받았을 때 해당 id의 데이터가 View로 전달되도록 만들어줍니다.
      */
 
+    @GetMapping("/post/edit/{id}")
+    public String edit(@PathVariable("id") Long id, Model model){
+        BoardDto boardDto = boardService.getPost(id);
+        model.addAttribute("post", boardDto);
+        return "board/edit.html";
+    }
+
+    /*
+    수정 버튼을 누르면 Put 형식으로 /post/edit/{id}로 서버에게 요청이 가게 됩니다. 서버에서 Put 요청이 오게 되면, 데이터베이스에 변경된 데이터를 저장해야 합니다.
+    BoardController 클래스에 update()를 구현하여 구현해보겠습니다.
+     */
+    @PutMapping("/post/edit/{id}")
+    public String update(BoardDto boardDto){
+        boardService.savePost(boardDto);
+        return "redirect:/";
+    }
+    /*
+    위 작업을 마치면 HiddenhttpMethodFilter를 Bean으로 등록하여, @PutMapping과 @DeleteMapping이 작동할 수 있도록 해줍니다.
+     */
 }
